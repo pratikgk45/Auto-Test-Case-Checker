bd7220bc81e3c9bc4d09c30afd77dacb34d8fc3d
Pratik Kale <kalepg45@gmail.com>
2019-05-05 21:38:27 +0530

    Add files via upload

--- scripts/generate_problem.py
+++ scripts/generate_problem.py
@@ -16,7 +16,11 @@ if response.status_code != 200:
 
 print "-> Response Code = "+str(response.status_code)
 html_content = html.document_fromstring(response.text)
-node = html_content.find_class("sample-test")[0]
+try:
+	node = html_content.find_class("sample-test")[0]
+except:
+	print "-> Test Case Fetching Failed"
+	quit()
 
 def filter_node(node,in_or_out):
 	node = node.getchildren()[1]
@@ -25,18 +29,18 @@ def filter_node(node,in_or_out):
 	node = node.replace("</pre>","")
 	node = node.replace("<br/>","\n")
 	# print node
-	filename = contest_id+'/tests/'+task_id+'_'+in_or_out+'_'+str(test_id) 
+	filename = "cf_"+contest_id+'/test_cases/'+task_id+'_'+in_or_out+'_'+str(test_id) 
 	f = file(filename,'w')
 	f.write(node)
 	f.close()
-	# print filename
 	subprocess.call("sed -i '/^$/d' "+filename,shell=True)
 
 num_samples = len(node.getchildren())/2
-f = file((contest_id+'/tests/'+task_id+'_num_samples'),'w')
+f = file(("cf_"+contest_id+'/test_cases/'+task_id+'_num_samples'),'w')
 f.write(str(num_samples))
 for test_id in range(num_samples):
 	test_input  = node[test_id*2]
 	test_input = filter_node(test_input,'in')
 	test_output = node[test_id*2+1]
 	test_output = filter_node(test_output,'out')
+print "-> Test Cases are Successfully Imported"
