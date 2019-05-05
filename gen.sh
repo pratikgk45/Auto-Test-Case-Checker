6c92f8cec3dc282453be965fa5598f27d1513324
Pratik Kale <kalepg45@gmail.com>
2019-05-05 21:38:04 +0530

    Add files via upload

--- gen.sh
+++ gen.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-read -p "Contest ID, problem count and Test Case import Flag : " id c f
-mkdir -p $id
-mkdir -p $id/tests/
-cd $id
+read -p "Enter Contest ID, problem count and Test Case import Flag : " id c f
+mkdir -p "cf_"$id
+mkdir -p "cf_"$id/test_cases/
+cd "cf_"$id
 ln -fs ../scripts/ver.sh ver.sh
 cd ..
 
 problem_count=$c
-test_import=$f
-echo $problem_count
-echo $test_import
+test_import_flag=$f
 for problem_num in $(seq 1 ${problem_count})
 do
 	problem_num=$(echo $problem_num+96 | bc)
 	problem=$(printf "\x$(printf %x $problem_num)")
 	echo -n $problem" "
-	cp -n --no-clobber template.cpp $id/$problem.cpp
-	if [[ "$test_import" == 1 ]];then
+	cp -n --no-clobber template.cpp "cf_"$id/$problem.cpp
+	if [[ "$test_import_flag" == 1 ]];then
 		python scripts/generate_problem.py $id $problem
 	fi
 done
