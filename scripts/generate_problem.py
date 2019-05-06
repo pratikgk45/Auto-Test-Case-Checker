8c360be7e49a7a48b044a7abdf815d81ee26b6a1
Pratik Kale <kalepg45@gmail.com>
2019-05-06 14:21:57 +0530

    Add files via upload

--- scripts/generate_problem.py
+++ scripts/generate_problem.py
@@ -11,7 +11,7 @@ task_url = "http://codeforces.com/contest/"+contest_id+"/problem/"+task_id
 
 response = requests.get(task_url)
 if response.status_code != 200:
-	print "Problem URL Fetching Failed"
+	print("Problem URL Fetching Failed")
 	sys.exit()
 
 # print "-> Response Code = "+str(response.status_code)
@@ -19,28 +19,24 @@ html_content = html.document_fromstring(response.text)
 try:
 	node = html_content.find_class("sample-test")[0]
 except:
-	print "-> Test Case Fetching Failed"
+	print("-> Test Case Fetching Failed")
 	quit()
 
 def filter_node(node,in_or_out):
 	node = node.getchildren()[1]
-	node = etree.tostring(node, pretty_print=True)
-	node = node.replace("<pre>","")
-	node = node.replace("</pre>","")
-	node = node.replace("<br/>","\n")
-	# print node
-	filename = "cf_"+contest_id+'/test_cases/'+task_id+'_'+in_or_out+'_'+str(test_id) 
-	f = file(filename,'w')
-	f.write(node)
+	filename = "cf_"+contest_id+'/test_cases/'+task_id+'_'+in_or_out+'_'+str(test_id)
+	f = open(filename,'w')
+	f.write(node.text_content())
 	f.close()
 	subprocess.call("sed -i '/^$/d' "+filename,shell=True)
 
-num_samples = len(node.getchildren())/2
-f = file(("cf_"+contest_id+'/test_cases/'+task_id+'_num_samples'),'w')
+# print(node.text_content())
+num_samples = int(len(node.getchildren())/2)
+f = open("cf_"+contest_id+'/test_cases/'+task_id+'_num_samples','w')
 f.write(str(num_samples))
 for test_id in range(num_samples):
 	test_input  = node[test_id*2]
 	test_input = filter_node(test_input,'in')
 	test_output = node[test_id*2+1]
 	test_output = filter_node(test_output,'out')
-print "-> Test Cases Successfully Imported"
+print("-> Test Cases Successfully Imported")
