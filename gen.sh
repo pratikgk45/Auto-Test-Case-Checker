ae1ef4a6f9b6b969adf7836794a9540f5c8e5c64
Pratik Kale <kalepg45@gmail.com>
2019-05-10 13:59:39 +0530

    Automatic Problem List Import

--- gen.sh
+++ gen.sh
@@ -1,62 +1,69 @@
 #!/bin/bash
-read -p "Enter Contest ID, problem count and Test Case import Flag : " id c f
+read -p "Enter Contest ID : " id
 mkdir -p "cf_"$id
 mkdir -p "cf_"$id/test_cases/
 cd "cf_"$id
 ln -fs ../scripts/ver.sh ver.sh
 cd ..
 
-problem_count=$c
-test_import_flag=$f
-for problem_num in $(seq 1 ${problem_count})
+pyth=""
+if command -v python3 &>/dev/null;then
+	pyth="python3"
+elif command -v python &>/dev/null; then
+	pyth="python"
+else
+	echo "Requirement python or python3 not satisfied"
+	exit 1
+fi
+
+f=1
+while [ 1 ];
 do
-	problem_num=$(echo $problem_num+96 | bc)
-	problem=$(printf "\x$(printf %x $problem_num)")
-	echo $problem" -> "$problem".cpp created "
-	if [ -f "cf_"$id/$problem.cpp ];then
-		rm "cf_"$id/$problem.cpp
+	if [[ "$f" -eq "6" ]];then
+		echo $'\n'"5 Tries completed. Terminating Program ... "
+		break
 	fi
-	cp -n --no-clobber template.cpp "cf_"$id/$problem.cpp
-done
+	echo "Try "$f" : "
+	f=$(( $f + 1 ))
 
-if [[ "$test_import_flag" == 1 ]];then
-	pyth=""
-	if command -v python3 &>/dev/null; then
-		pyth="python3"
-	elif command -v python &>/dev/null; then
-		pyth="python"
-	else
-		echo "Requirement python or python3 not satisfied"
-		exit 1
-	fi 
+	problems=$(${pyth} scripts/problem_list_import.py $id)
 
-	f=1
-	while [ 1 ];
+	if [[ "$?" -eq 0 ]];then
+		echo "Problems are not released yet"
+		continue
+	fi
+
+	declare -a problem_list
+	for i in $problems;
 	do
-		echo -n "Test Case Fetching Try "$f" : "
-		c=0
-		for problem_num in $(seq 1 ${problem_count})
-		do
-			problem_num=$(echo $problem_num+96 | bc)
-			problem=$(printf "\x$(printf %x $problem_num)")
-			echo -n $problem" "
-			${pyth} scripts/test_case_import.py $id $problem
-			c=$(( $c + $? ))
-		done
-		if [[ "$c" -eq "${problem_count}" ]];then
-			echo $'\n'"Test Case Fetching Complete :)"
-			break
-		elif [[ "$c" -ne 0 ]];then
-			echo $'\n'"Partial Test Case Fetching Done !"
-			break
-		else
-			echo $'\n'"Test Case Fetching Failed :("
-		fi
+		problem_list+=($i)
+	done
+	problem_count=${#problem_list[@]}
 
-		f=$(( $f + 1 ))
-		if [[ "$f" -eq "6" ]];then
-			echo $'\n'"5 Tries completed. Terminating Program ... "
-			break
+	for problem in ${problem_list[@]};
+	do
+		if [ -f "cf_"$id/$problem".cpp" ];then
+			echo $problem" -> "$problem".cpp already exists"
+		else
+			echo $problem" -> "$problem".cpp created"
+			cp -n --no-clobber template.cpp "cf_"$id/$problem.cpp
 		fi
 	done
-fi
+
+	echo -n "Test Case Fetching : "
+	c=0
+	for problem in ${problem_list[@]};
+	do
+		echo -n $problem" "
+		${pyth} scripts/test_case_import.py $id $problem
+		c=$(( $c + $? ))
+	done
+	if [[ "$c" -eq "${problem_count}" ]];then
+		echo $'\n'"Test Case Fetching Complete :)"
+		break
+	elif [[ "$c" -ne 0 ]];then
+		echo $'\n'"Partial Test Case Fetching Done !"
+	else
+		echo $'\n'"Test Case Fetching Failed :("
+	fi
+done
