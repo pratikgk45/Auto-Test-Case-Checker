7d4f56bda129041fbce49f9b6da5fbd971eae3de
Pratik Kale <kalepg45@gmail.com>
2019-05-10 19:22:00 +0530

    Add files via upload

--- gen.sh
+++ gen.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 read -p "Enter Contest ID : " id
-mkdir -p "cf_"$id
-mkdir -p "cf_"$id/test_cases/
-cd "cf_"$id
-ln -fs ../scripts/ver.sh ver.sh
-cd ..
+if [[ -z "$id" || -n ${id//[0-9]/} ]];then
+	echo "Please enter valid integer ID"
+	exit 1
+fi
 
 pyth=""
 if command -v python3 &>/dev/null;then
@@ -16,54 +15,51 @@ else
 	exit 1
 fi
 
-f=1
-while [ 1 ];
+problems=$(${pyth} scripts/problem_list_import.py $id)
+
+if [[ "$?" -eq 0 ]];then
+	echo "Problems are not released yet"
+	exit 1
+fi
+
+mkdir -p "cf_"$id
+mkdir -p "cf_"$id/test_cases/
+cd "cf_"$id
+ln -fs ../scripts/ver.sh ver.sh
+ln -fs ../scripts/run.sh run.sh
+cd ..
+
+declare -a problem_list
+for i in $problems;
 do
-	if [[ "$f" -eq "6" ]];then
-		echo $'\n'"5 Tries completed. Terminating Program ... "
-		break
-	fi
-	echo "Try "$f" : "
-	f=$(( $f + 1 ))
+	problem_list+=($i)
+done
+problem_count=${#problem_list[@]}
 
-	problems=$(${pyth} scripts/problem_list_import.py $id)
-
-	if [[ "$?" -eq 0 ]];then
-		echo "Problems are not released yet"
-		continue
-	fi
-
-	declare -a problem_list
-	for i in $problems;
-	do
-		problem_list+=($i)
-	done
-	problem_count=${#problem_list[@]}
-
-	for problem in ${problem_list[@]};
-	do
-		if [ -f "cf_"$id/$problem".cpp" ];then
-			echo $problem" -> "$problem".cpp already exists"
-		else
-			echo $problem" -> "$problem".cpp created"
-			cp -n --no-clobber template.cpp "cf_"$id/$problem.cpp
-		fi
-	done
-
-	echo -n "Test Case Fetching : "
-	c=0
-	for problem in ${problem_list[@]};
-	do
-		echo -n $problem" "
-		${pyth} scripts/test_case_import.py $id $problem
-		c=$(( $c + $? ))
-	done
-	if [[ "$c" -eq "${problem_count}" ]];then
-		echo $'\n'"Test Case Fetching Complete :)"
-		break
-	elif [[ "$c" -ne 0 ]];then
-		echo $'\n'"Partial Test Case Fetching Done !"
+for problem in ${problem_list[@]};
+do
+	if [ -f "cf_"$id/$problem".cpp" ];then
+		echo $problem" -> "$problem".cpp already exists"
 	else
-		echo $'\n'"Test Case Fetching Failed :("
+		echo $problem" -> "$problem".cpp created"
+		cp -n --no-clobber template.cpp "cf_"$id/$problem.cpp
 	fi
 done
+
+echo -n "Test Case Fetching : "
+c=0
+for problem in ${problem_list[@]};
+do
+	echo -n $problem" "
+	${pyth} scripts/test_case_import.py $id $problem
+	c=$(( $c + $? ))
+done
+if [[ "$c" -eq "${problem_count}" ]];then
+	echo $'\n'"Test Case Fetching Complete :)"
+elif [[ "$c" -ne 0 ]];then
+	echo $'\n'"Partial Test Case Fetching Done !"
+	echo "Please Try again"
+else
+	echo $'\n'"Test Case Fetching Failed :("
+	echo "Please Try again"
+fi
