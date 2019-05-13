14007e832c0d82f85605d041faecf132e71f3020
Pratik Kale <kalepg45@gmail.com>
2019-05-14 01:26:04 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,5 +8,5 @@ cd scripts
 alias ver=./ver.sh
 alias run=./run.sh
 mkdir MathJax-2.7.3
-git clone https://github.com/mathjax/MathJax.git ./scripts/MathJax-2.7.3/
+git clone https://github.com/mathjax/MathJax.git ./MathJax-2.7.3/
 cd ..
