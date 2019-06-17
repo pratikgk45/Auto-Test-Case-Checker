c894b46beae295803964b2af4da2602cc7a0cf99
Pratik Kale <kalepg45@gmail.com>
2019-06-17 11:37:46 +0530

    Submit Validation Fixed

--- static/submit.py
+++ static/submit.py
@@ -11,11 +11,16 @@ task_id = (sys.argv)[2]
 
 url = "https://codeforces.com/contest/"+contest_id+"/problem/"+task_id
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
-driver.get(url)
 try:
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument('--headless')
+	driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+except:
+	print("<span id='close_pop_up'>&times;</span><h5 style='color:red;'>Chrome Webdriver Error</h5><p>Chrome webdriver cannot be initiated</p><p>For manual submit, <a href='https://codeforces.com/contest/"+str(contest_id)+"/submit/"+task_id+"' target='_blank' style='color:#7fff00;font-weight:bold;text-decoration:none;'>CLICK HERE</a></p>")
+	sys.exit()
+	
+try:
+	driver.get(url)
 	enter = driver.find_element(By.LINK_TEXT, "Enter")
 	enter.click()
 	user_name = driver.find_element_by_id("handleOrEmail")
@@ -26,18 +31,35 @@ try:
 	submit.click()
 	time.sleep(10)
 except:
-	pass
+	print("<span id='close_pop_up'>&times;</span><h5 style='color:#fa4a4a;'>Network Error / Site is not available</h5>")
+	sys.exit()
+	
 
-language = driver.find_element_by_name("programTypeId")
-language.send_keys("GNU G++17 7.3.0")
-sourceFile = driver.find_element_by_name("sourceFile")
-sourceFile.send_keys(os.path.abspath("contests/cf_"+str(contest_id)+"/"+task_id+".cpp"))
-submit_btn = driver.find_element_by_class_name("submit")
-submit_btn.click()
+flag = 0
 try:
 	error_source_file = driver.find_element_by_class_name("shiftUp")
-	print("<span id='close_pop_up'>&times;</span><h5 style='color:red;'>Error Occured</h5><p>You are trying to submit exactly the same code before</p><p>Please visit <a href='https://codeforces.com/contest/"+str(contest_id)+"/my' target='_blank' style='color:green;font-weight:bold;text-decoration:none;'>My Submissions</a></p>")
+	flag = 1
 except:
-	print("<span id='close_pop_up'>&times;</span><h5>Solution Submitted</h5><p>Please visit <a href='https://codeforces.com/contest/"+str(contest_id)+"/my' target='_blank' style='color:green;font-weight:bold;'>My Submissions</a></p>")
+	pass
+if flag:
+	print("<span id='close_pop_up'>&times;</span><h5 style='color:#fa4a4a;'>Invalid Login Credentials</h5><p>Please enter valid credentials in static/credentials.py</p>")
+	sys.exit()
+
+try:
+	language = driver.find_element_by_name("programTypeId")
+	language.send_keys("GNU G++17 7.3.0")
+	sourceFile = driver.find_element_by_name("sourceFile")
+	sourceFile.send_keys(os.path.abspath("contests/cf_"+str(contest_id)+"/"+task_id+".cpp"))
+	submit_btn = driver.find_element_by_class_name("submit")
+	submit_btn.click()
+except:
+	print("<span id='close_pop_up'>&times;</span><h5 style='color:#fa4a4a;'>Submission Error</h5><p>"+task_id+".cpp cannot be submitted</p><p>For manual submit, <a href='https://codeforces.com/contest/"+str(contest_id)+"/submit/"+task_id+"' target='_blank' style='color:#7fff00;font-weight:bold;text-decoration:none;'>CLICK HERE</a></p>")
+	sys.exit()
+
+try:
+	error_source_file = driver.find_element_by_class_name("shiftUp")
+	print("<span id='close_pop_up'>&times;</span><h5 style='color:#fa4a4a;'>Same Solution Error</h5><p>You are trying to submit exactly the same code before</p><p>Please visit <a href='https://codeforces.com/contest/"+str(contest_id)+"/my' target='_blank' style='color:#7fff00;font-weight:bold;text-decoration:none;'>My Submissions</a></p>")
+except:
+	print("<span id='close_pop_up'>&times;</span><h5>Solution Submitted</h5><p>Please visit <a href='https://codeforces.com/contest/"+str(contest_id)+"/my' target='_blank' style='color:#7fff00;font-weight:bold;'>My Submissions</a></p>")
 
 driver.close()
